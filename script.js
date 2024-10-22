7028bc96288e7a2de8bfbf9f424c21686f58503c
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-22 22:24:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
+        function getFormvalue() {
+            // Retrieve the first and last name from the form inputs
+            const firstName = document.getElementById('firstName').value;
+            const lastName = document.getElementById('lastName').value;
 
-}
+            // Display the full name in an alert box
+            alert(firstName + " " + lastName);
+        }
